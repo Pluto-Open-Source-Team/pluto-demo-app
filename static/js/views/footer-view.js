e598ac9b22aa3a56d32770508d9b084cde2fa45a
Julian Felix Rost <j-frost+github@a3re.net>
2022-03-07 14:34:22 +0000

    Add /privacy and /tos links to footer

--- static/js/views/footer-view.js
+++ static/js/views/footer-view.js
@@ -3,7 +3,7 @@ const Footer = {
      * Render the component content.
      */
     render: async () => {
-        return `<p><span class="footer-part-1">&copy; 2022 Pluto. All Rights Reserved</span><span class="footer-part-2"><a href="#">Privacy Policy</a> | <a href="#">Terms & Conditions</a></span></p>`;
+        return `<p><span class="footer-part-1">&copy; 2022 Pluto. All Rights Reserved</span><span class="footer-part-2"><a href="/privacy">Privacy Policy</a> | <a href="/tos">Terms & Conditions</a></span></p>`;
     },
 
     /**
