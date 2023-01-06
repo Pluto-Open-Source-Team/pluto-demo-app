bf84e3d154b1a0819fc409249cd7dbff73e44ba9
Julian Felix Rost <j-frost+github@a3re.net>
2023-01-06 15:55:31 +0000

    Add dark mode Github logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import githubLogo from '$lib/assets/github-mark-white.svg';
+	import githubLogoWhite from '$lib/assets/github-mark-white.svg';
+	import githubLogo from '$lib/assets/github-mark.svg';
 	import IconButton from '@smui/icon-button';
 	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+	import { onMount } from 'svelte';
 	import { pageTitle } from './stores';
 
 	export const prerender = true;
+
+	let darkMode: boolean;
+	onMount(() => {
+		darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	});
 </script>
 
 <TopAppBar variant="static" prominent={false} dense={false} color={'primary'}>
@@ -26,7 +33,11 @@
 						?.focus();
 				}}
 			>
-				<img src={githubLogo} alt="Github logo" />
+				{#if darkMode}
+					<img src={githubLogoWhite} alt="Github logo" />
+				{:else}
+					<img src={githubLogo} alt="Github logo" />
+				{/if}
 			</IconButton>
 		</Section>
 	</Row>
